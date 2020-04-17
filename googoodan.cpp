057525e40b701cd61339f329d8d6786b13bda1bb
SunJin Park <tjswlsdl135@naver.com>
2020-04-17 10:58:56 +0900

    add 9 times table

--- googoodan.cpp
+++ googoodan.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    for(int i=1; i<=8; i++) {
+    for(int i=1; i<=9; i++) {
         cout<< i<<"단"<<endl;
         for(int j=1; j<=9; j++) {
             cout<< i <<" * "<< j<<" = "<< i*j<<endl;
